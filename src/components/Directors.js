27f213843a9020500327c1701915df9f89064c94
raaynaldo <raaynaldo@hotmail.com>
2020-12-28 23:15:21 -0500

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import { directors } from '../data';
+import React from "react";
+import { directors } from "../data";
 
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map((directors, idx) => {
+        return (
+          <div key={idx}>
+            <h3>Name: {directors.name}</h3>
+            <p>Movies:</p>
+            <ul>
+              {directors.movies.map((movie, idx) => {
+                return <li key={idx}>{movie}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
-export default Directors
+export default Directors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/movies">Movies</NavLink>
+        </li>
+        <li>
+          <NavLink to="/directors">Directors</NavLink>
+        </li>
+        <li>
+          <NavLink to="/actors">Actors</NavLink>
+        </li>
+      </ul>
     </div>
   );
 };
