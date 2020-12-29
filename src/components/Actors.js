4736de25128dd0e444fbb8dd7e92f290253ab1a3
raaynaldo <raaynaldo@hotmail.com>
2020-12-28 23:15:18 -0500

    commit tree ef9a2bf8b018b4e56404fc3c789f0f83ea93eb57

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import { actors } from '../data';
+import React from "react";
+import { actors } from "../data";
 
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor, idx) => {
+        return (
+          <div key={idx}>
+            <h2>Name: {actor.name}</h2>
+            <p>Movies: </p>
+            <ul>
+              {actor.movies.map((movie, idx) => (
+                <li key={idx}>{movie}</li>
+              ))}
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
 
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie, idx) => {
+        return (
+          <div key={idx}>
+            <h2>Name: {movie.title}</h2>
+            <p>Time: {movie.time}</p>
+            <p>genres</p>
+            <ul>
+              {movie.genres.map((genre, idx) => {
+                return <li key={idx}>{genre}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 };
